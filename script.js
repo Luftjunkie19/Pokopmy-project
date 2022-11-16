b39253919bb9f38b9c6a8e470146ff393059a6d8
Łukasz Borkowski <krawczyk0606@gmail.com>
2022-11-16 19:28:58 +0000

    Big changes

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ const mapEl = document.querySelector("#map");
 const navigationBtns = document.querySelectorAll(".btn.nav");
 const optionsBtns = document.querySelectorAll(".btn.option");
 const contentBoxes = document.querySelectorAll(".content-box");
-const mapContainer = document.querySelector(".map-container");
-const selectEl = document.querySelector("select");
-const personNeededEl = document.querySelector(".persons-needed");
-const increaseBtn = document.querySelector(".increase-btn");
-const decreaseBtn = document.querySelector(".decrease-btn");
 const placeFormFields = document.querySelectorAll(".place-data");
 const eventFormFields = document.querySelectorAll(".event-data");
-const descriptionData = document.querySelector("textarea");
+const matchFormFields = document.querySelectorAll(".match-data");
 const sideOptions = document.querySelectorAll(".side-option");
+const selectElements = document.querySelectorAll("select");
+const controlFields = document.querySelectorAll(".control-field");
+const mapContainer = document.querySelector(".map-container");
+const selectType = document.querySelector("#type");
+const personNeededEl = document.querySelector("#persons-needed");
+const increaseBtn = document.querySelector(".increase-btn");
+const decreaseBtn = document.querySelector(".decrease-btn");
+const description = document.querySelector("textarea");
+const submitBtn = document.querySelector(".submit-btn");
+const descriptionEl = document.querySelector(".description-data");
 
-console.log(placeFormFields, eventFormFields);
-
+//Text-fields
+const placeNameInput = document.querySelector("#placename");
+const latidudeInput = document.querySelector("#latidude");
+const longitudeInput = document.querySelector("#longitude");
+const kindOfEventInput = document.querySelector("#kind-event");
+const eventNameInput = document.querySelector("#event-name");
+const homeTeamInput = document.querySelector("#home-team");
+const opponentTeamInput = document.querySelector("#enemy-team");
+//Sidebar Containers
+const eventSideOption = document.querySelector(".side-option.events");
 let footballPlaygrounds = [
   {
     lat: 53.36905552600669,
@@ -91,6 +104,25 @@ let footballPlaygrounds = [
     popup: "Komin Arena, Boisko Kasty Majowe, Szczecin Prawobrzeże.",
   },
 ];
+let MatchesArray = [];
+let EventsArray = [];
+let academyArray = [];
+
+function clearSelects() {
+  selectElements.forEach((element) => {
+    element.value = "";
+  });
+}
+clearSelects();
+
+function hideFirstControls() {
+  for (let index = 1; index < controlFields.length; index++) {
+    const element = controlFields[index];
+    element.style.display = `none`;
+  }
+}
+
+hideFirstControls();
 
 function success(position) {
   let latidude = position.coords.latitude;
@@ -159,6 +191,17 @@ function displayFlex(boxes) {
   });
 }
 
+function clearInputs(boxes) {
+  boxes.forEach((box) => {
+    const input = box.children[0].children[1];
+    input.value = "";
+  });
+}
+
+function clearDescription() {
+  description.value = "";
+}
+
 optionsBtns.forEach((btn, i) => {
   hideOn(sideOptions);
   btn.addEventListener("click", () => {
@@ -186,10 +229,18 @@ navigationBtns.forEach((btn, i) => {
 });
 
 function showSuitableForm() {
-  if (selectEl.value === "event" || selectEl.value === "match") {
+  descriptionEl.style.display = `flex`;
+
+  if (selectType.value === "event") {
+    hideOn(placeFormFields);
+    hideOn(matchFormFields);
+    displayFlex(eventFormFields);
+  } else if (selectType.value === "match") {
     hideOn(placeFormFields);
     displayFlex(eventFormFields);
+    displayFlex(matchFormFields);
   } else {
+    hideOn(matchFormFields);
     hideOn(eventFormFields);
     displayFlex(placeFormFields);
   }
@@ -202,7 +253,7 @@ let quantityOfPlayers = 0;
 function increasePlayers(e) {
   e.preventDefault();
   quantityOfPlayers++;
-  personNeededEl.innerText = quantityOfPlayers;
+  personNeededEl.value = quantityOfPlayers;
 }
 
 function decreasePlayers(e) {
@@ -211,10 +262,150 @@ function decreasePlayers(e) {
     quantityOfPlayers = 0;
   } else {
     quantityOfPlayers--;
-    personNeededEl.innerText = quantityOfPlayers;
+    personNeededEl.value = quantityOfPlayers;
   }
 }
 
+function showEmpty(element) {
+  const messageField = element.parentElement.nextElementSibling;
+
+  messageField.classList.add("bad");
+  messageField.innerText = `Apologies, but the ${element.id}-input is invalid`;
+}
+
+function clearEventInputs() {
+  for (let i = 0; i < 2; i++) {
+    const input = eventFormFields[i].children[0].children[1];
+    input.value = "";
+  }
+}
+
+function removeBadClass(element) {
+  const messageField = element.parentElement.nextElementSibling;
+  console.log(messageField);
+
+  messageField.classList.remove("bad");
+  messageField.innerText = ``;
+}
+
+function alertZero() {
+  const messageField = personNeededEl.parentElement.nextElementSibling;
+
+  messageField.classList.add("bad");
+  messageField.innerText = `Increase the quantity of players`;
+}
+
+class Event {
+  constructor(name, sort, persons, desciption) {
+    this.name = name;
+    this.sort = sort;
+    this.persons = persons;
+    this.desciption = desciption;
+  }
+
+  addToDOM() {
+    const eventDiv = document.createElement("div");
+    eventDiv.classList.add("event");
+    eventDiv.innerHTML = ` 
+    <i class="fas fa-soccer-ball fa-3x"></i>
+    <p class="name">${this.name}</p>
+    <p class="sort">${this.sort}</p>
+    <p class="quantity">Needed: ${this.persons}</p>`;
+    eventSideOption.append(eventDiv);
+  }
+}
+
+class Place {
+  constructor(latidude, longitude, name, desciption) {
+    this.latidude = latidude;
+    this.longitude = longitude;
+    this.name = name;
+    this.desciption = desciption;
+  }
+}
+
+class Match extends Event {
+  constructor(name, home, enemy, persons, description) {
+    super(name, description, persons);
+    this.home = home;
+    this.enemy = enemy;
+  }
+}
+
+function addEvent() {
+  if (selectType.value === "event") {
+    if (kindOfEventInput.value.trim("") === "") {
+      showEmpty(kindOfEventInput);
+    }
+
+    if (eventNameInput.value.trim("") === "") {
+      showEmpty(eventNameInput);
+    }
+
+    if (personNeededEl.value.trim("") === "") {
+      alertZero();
+    } else {
+      console.log(eventNameInput.value);
+      removeBadClass(personNeededEl);
+      removeBadClass(kindOfEventInput);
+      removeBadClass(eventNameInput);
+
+      const newEvent = new Event(
+        eventNameInput.value,
+        kindOfEventInput.value,
+        +personNeededEl.value,
+        description.value
+      );
+
+      console.log(newEvent);
+      newEvent.addToDOM();
+
+      quantityOfPlayers = 0;
+      personNeededEl.value = "";
+      clearEventInputs();
+      clearDescription();
+    }
+  }
+}
+
+function addMatch() {
+  if (selectType.value === "match") {
+  }
+}
+
+function addPlace() {
+  if (selectType.value === "academy" || selectType.value === "playground") {
+    if (isNaN(+latidudeInput.value) || latidudeInput.value.trim("") === "") {
+      showEmpty(latidudeInput);
+    }
+
+    if (isNaN(+longitudeInput.value) || longitudeInput.value.trim("") === "") {
+      showEmpty(longitudeInput);
+    } else {
+      removeBadClass(latidudeInput);
+      removeBadClass(longitudeInput);
+      removeBadClass(placeNameInput);
+
+      const newPlace = new Place(
+        +latidudeInput.value,
+        +longitudeInput.value,
+        placeNameInput.value,
+        description.value
+      );
+
+      console.log(newPlace);
+      clearInputs(placeFormFields);
+      clearDescription();
+    }
+  }
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addPlace();
+  addEvent();
+});
+
 increaseBtn.addEventListener("click", increasePlayers);
 decreaseBtn.addEventListener("click", decreasePlayers);
-selectEl.addEventListener("change", showSuitableForm);
+selectType.addEventListener("change", showSuitableForm);
