8d1130150b3c277af73e6d96ab3321530d4b27b9
Łukasz Borkowski <krawczyk0606@gmail.com>
2022-11-18 17:53:14 +0000

    Implementing tiny changes

--- script.js
+++ script.js
@@ -23,85 +23,106 @@ const latidudeInput = document.querySelector("#latidude");
 const longitudeInput = document.querySelector("#longitude");
 const kindOfEventInput = document.querySelector("#kind-event");
 const eventNameInput = document.querySelector("#event-name");
-const homeTeamInput = document.querySelector("#home-team");
-const opponentTeamInput = document.querySelector("#enemy-team");
+const homeImgInput = document.querySelector("#home-team");
+const opponentImgInput = document.querySelector("#enemy-team");
+const homeNameInput = document.querySelector("#home-name");
+const opponentNameInput = document.querySelector("#opponent-name");
+const matchDatePicker = document.querySelector("#match-date");
+
+const homeLogoImg = document.querySelector(".home-logo");
+const opponentLogoImg = document.querySelector(".enemy-logo");
 //Sidebar Containers
 const eventSideOption = document.querySelector(".side-option.events");
+const matchSideOption = document.querySelector(".side-option.matches");
+const playgroundSideOption = document.querySelector(".side-option.playgrounds");
+const academySideOption = document.querySelector(".side-option.academy");
+
 let footballPlaygrounds = [
   {
     lat: 53.36905552600669,
     lng: 14.650691305007095,
-    popup: "Boisko pełnowymiarowe, MORiS Szczecin Prawobrzeże.",
+    name: "Boisko pełnowymiarowe, MORiS Szczecin Prawobrzeże.",
+    description: "",
   },
   {
     lat: 53.36846178739087,
     lng: 14.651112644164295,
-    popup: "Orlik ze sztuczną nawierzchnią, MORiS Szczecin Prawobrzeże.",
+    name: "Orlik, MORiS Szczecin Prawobrzeże.",
+    description: "Orlik ze sztuczną nawierzchnią",
   },
   {
     lat: 53.36320740015133,
     lng: 14.656772988084755,
-    popup:
-      "Orlik ze sztuczną nawierzchnią, SP 74 im. Stanisława Grońskiego, Szczecinie.",
+    name: "Orlik, SP 74 im. Stanisława Grońskiego, Szczecin.",
+    description: "Orlik ze sztuczną nawierzchnią",
   },
   {
     lat: 53.3683673645842,
     lng: 14.665738361565003,
-    popup:
-      "Boisko z trawiastą nawierzchnią (niepełnowymiarowe), za Intermarche, Szczecin.",
+    name: "Boisko, za Intermarche, Szczecin.",
+    desciption: "Boisko z trawiastą nawierzchnią (niepełnowymiarowe)",
   },
   {
     lat: 53.3747268330635,
     lng: 14.666672423592805,
-    popup:
-      "Orlik ze sztuczną nawierzchnią, SP 37 im. Antoniego Ledóchowskiego, Szczecin.",
+    name: "Orlik, SP 37 im. Antoniego Ledóchowskiego, Szczecin.",
+    description: "Orlik ze sztuczną nawierzchnią.",
   },
   {
     lat: 53.38065337408911,
     lng: 14.656376928049148,
-    popup: "Orlik ze sztuczną nawierzchnią przy CKS, Szczecin Prawobrzeże.",
+    name: "Orlik przy CKS, Szczecin Prawobrzeże.",
+    description: "Orlik ze sztuczną nawierzchnią.",
   },
   {
     lat: 53.375301282827756,
     lng: 14.690769425627927,
-    popup: "Pełnowymiarowe Boisko Aquilli Szczecin, Szczecin Prawobrzeże.",
+    name: "Boisko Aquilli Szczecin, Szczecin Prawobrzeże.",
+    description: "Pełnowymiarowe Boisko Aquilli Szczecin",
   },
   {
     lat: 53.38242605601696,
     lng: 14.638423296454903,
-    popup:
-      "Orlik ze sztuczną nawierzchnią przy SP 65 im. Antoniego Bolesława Dobrowolskiego, Szczecin Prawobrzeże.",
+    name: "Orlik przy SP 65, Szczecin Prawobrzeże.",
+    desciption:
+      "Orlik ze sztuczną nawierzchnią przy SP 65 im. Antoniego Bolesława Dobrowolskiego",
   },
   {
     lat: 53.394557666577896,
     lng: 14.673681354904422,
-    popup: "Orlik ze sztuczną nawierzchnią przy III LO, Szczecin Prawobrzeże.",
+    name: "Orlik przy III LO, Szczecin Prawobrzeże.",
+    description: "Orlik ze sztuczną nawierzchnią przy III LO",
   },
   {
     lat: 53.40355016657418,
     lng: 14.683832184132953,
-    popup:
-      "Orlik ze sztuczną nawierzchnią SP 71 im. Bogusława X i Anny Jagielonki, Szczecin Prawobrzeże.",
+    name: "Orlik przy SP 71, Szczecin Prawobrzeże.",
+    description:
+      "Orlik ze sztuczną nawierzchnią SP 71 im. Bogusława X i Anny Jagielonki",
   },
   {
     lat: 53.364586126592314,
     lng: 14.5958746,
-    popup: "Orlik przy SP 12, Szczecin Prawobrzeże.",
+    name: "Orlik przy SP 12, Szczecin Prawobrzeże.",
+    description: "Orlik przy SP 12, Szczecin Prawobrzeże.",
   },
   {
     lat: 53.43776572154318,
     lng: 14.745676436553724,
-    popup: "Boisko OKS Jeziorak, Szczecin Prawobrzeże.",
+    name: "Boisko OKS Jeziorak, Szczecin Prawobrzeże.",
+    description: "Boisko OKS Jeziorak, Szczecin Prawobrzeże.",
   },
   {
     lat: 53.34171384117264,
     lng: 14.76252451756204,
-    popup: "Boisko Iskierki Szczecin, Szczecin Prawobrzeże.",
+    name: "Boisko Iskierki Szczecin, Szczecin Prawobrzeże.",
+    description: "Boisko OKS Jeziorak, Szczecin Prawobrzeże.",
   },
   {
     lat: 53.37339675697874,
     lng: 14.674364563732393,
-    popup: "Komin Arena, Boisko Kasty Majowe, Szczecin Prawobrzeże.",
+    name: "Komin Arena, Boisko Kasty Majowe, Szczecin Prawobrzeże.",
+    description: "Komin Arena, Boisko Kasty Majowe, Szczecin Prawobrzeże.",
   },
 ];
 let MatchesArray = [];
@@ -136,11 +157,13 @@ function success(position) {
 function showMarkers(ground, map) {
   let marker = L.marker([ground.lat, ground.lng]).addTo(map);
 
-  marker.bindPopup(`${ground.popup}`);
+  marker.bindPopup(`${ground.name}`);
 }
 
+let map;
+
 function showMap(lat, lng) {
-  let map = L.map("map").setView([lat, lng], 14);
+  map = L.map("map").setView([lat, lng], 14);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
@@ -237,7 +260,7 @@ function showSuitableForm() {
     displayFlex(eventFormFields);
   } else if (selectType.value === "match") {
     hideOn(placeFormFields);
-    displayFlex(eventFormFields);
+    hideOn(eventFormFields);
     displayFlex(matchFormFields);
   } else {
     hideOn(matchFormFields);
@@ -266,11 +289,15 @@ function decreasePlayers(e) {
   }
 }
 
-function showEmpty(element) {
+function showValid(element) {
   const messageField = element.parentElement.nextElementSibling;
 
-  messageField.classList.add("bad");
-  messageField.innerText = `Apologies, but the ${element.id}-input is invalid`;
+  if (element.value.trim("") === "") {
+    messageField.classList.add("bad");
+    messageField.innerText = `Apologies, but the ${element.id}-input is invalid`;
+  } else {
+    removeBadClass(element);
+  }
 }
 
 function clearEventInputs() {
@@ -288,12 +315,56 @@ function removeBadClass(element) {
   messageField.innerText = ``;
 }
 
-function alertZero() {
-  const messageField = personNeededEl.parentElement.nextElementSibling;
+function numberValidation(element, regex) {
+  const messageField = element.parentElement.nextElementSibling;
+  const fieldToNumber = +element.value;
 
-  messageField.classList.add("bad");
-  messageField.innerText = `Increase the quantity of players`;
+  if (
+    isNaN(fieldToNumber) ||
+    fieldToNumber === 0 ||
+    !element.value.match(regex)
+  ) {
+    messageField.classList.add("bad");
+    messageField.innerText = `Apologies, but your ${element.id} is not a valid, please change it.`;
+  } else {
+    removeBadClass(element);
+  }
 }
+
+function alertZero() {
+  if (quantityOfPlayers === 0) {
+    const messageField = personNeededEl.parentElement.nextElementSibling;
+
+    messageField.classList.add("bad");
+    messageField.innerText = `Increase the quantity of players`;
+  } else {
+    removeBadClass(personNeededEl);
+  }
+}
+
+function addToDOM() {
+  footballPlaygrounds.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList.add("playground");
+    div.innerHTML = `
+    <div class="small-logo home">
+      <img
+        src="./Image/pitch (1).png"
+        alt="Football-playground"
+      />
+    </div>
+    
+    <p class="playground-name">${element.name}</p>
+
+    <div class="coords">
+    <small class="coord">Latidude: ${element.lat}</small>
+    <small class="coord">Longitude: ${element.lng}</small>
+    </div>
+    `;
+    playgroundSideOption.append(div);
+  });
+}
+addToDOM();
 
 class Event {
   constructor(name, sort, persons, desciption) {
@@ -301,6 +372,10 @@ class Event {
     this.sort = sort;
     this.persons = persons;
     this.desciption = desciption;
+  }
+
+  addEventToArray(element) {
+    EventsArray.push(element);
   }
 
   addToDOM() {
@@ -316,39 +391,94 @@ class Event {
 }
 
 class Place {
-  constructor(latidude, longitude, name, desciption) {
-    this.latidude = latidude;
-    this.longitude = longitude;
+  constructor(lat, lng, name, desciption) {
+    this.lat = lat;
+    this.lng = lng;
     this.name = name;
     this.desciption = desciption;
+  }
+
+  addPlaceToArray(element) {
+    footballPlaygrounds.push(element);
   }
 }
 
 class Match extends Event {
-  constructor(name, home, enemy, persons, description) {
+  constructor(
+    name,
+    home,
+    enemy,
+    homeImg,
+    opponentImg,
+    persons,
+    date,
+    description
+  ) {
     super(name, description, persons);
     this.home = home;
     this.enemy = enemy;
+    this.homeImg = homeImg;
+    this.opponentImg = opponentImg;
+    this.date = date;
+  }
+
+  addMatchToArray(element) {
+    MatchesArray.push(element);
+  }
+
+  addMatchToDOM() {
+    const div = document.createElement("div");
+    div.classList.add("match");
+    div.innerHTML = `  
+    <div class="teams">
+    <div class="small-logo home">
+      <img
+        src="${this.homeImg}"
+        alt="Home-img"
+      />
+    </div>
+    <p class="versus">VS</p>
+    <div class="small-logo opponent">
+      <img
+        src="${this.opponentImg}"
+        alt="Away-img"
+      />
+    </div>
+  </div>
+  <p class="name">${this.home} vs ${this.enemy}</p>
+  <p class="game-date">${this.date}</p>`;
+
+    matchSideOption.append(div);
   }
 }
 
+//Input messages
+const kindOfEventMsg = kindOfEventInput.parentElement.nextElementSibling;
+const eventNameInputMsg = eventNameInput.parentElement.nextElementSibling;
+const personNeededElMsg = personNeededEl.parentElement.nextElementSibling;
+const latidudeInputMsg = latidudeInput.parentElement.nextElementSibling;
+const longitudeInputMsg = longitudeInput.parentElement.nextElementSibling;
+const placeNameInputMsg = placeNameInput.parentElement.nextElementSibling;
+const homeImgInputMsg = homeImgInput.parentElement.nextElementSibling;
+const opponentImgInputMsg = opponentImgInput.parentElement.nextElementSibling;
+const homeNameInputMsg = homeNameInput.parentElement.nextElementSibling;
+const opponentNameInputMsg = opponentNameInput.parentElement.nextElementSibling;
+const matchDatePickerMsg = matchDatePicker.parentElement.nextElementSibling;
+
 function addEvent() {
   if (selectType.value === "event") {
-    if (kindOfEventInput.value.trim("") === "") {
-      showEmpty(kindOfEventInput);
-    }
+    showValid(kindOfEventInput);
+    showValid(eventNameInput);
+    alertZero();
 
-    if (eventNameInput.value.trim("") === "") {
-      showEmpty(eventNameInput);
-    }
-
-    if (personNeededEl.value.trim("") === "") {
-      alertZero();
+    if (
+      kindOfEventMsg.classList.contains("bad") ||
+      eventNameInputMsg.classList.contains("bad") ||
+      personNeededElMsg.classList.contains("bad")
+    ) {
+      return;
     } else {
       console.log(eventNameInput.value);
-      removeBadClass(personNeededEl);
-      removeBadClass(kindOfEventInput);
-      removeBadClass(eventNameInput);
 
       const newEvent = new Event(
         eventNameInput.value,
@@ -357,7 +487,6 @@ function addEvent() {
         description.value
       );
 
-      console.log(newEvent);
       newEvent.addToDOM();
 
       quantityOfPlayers = 0;
@@ -368,30 +497,116 @@ function addEvent() {
   }
 }
 
+function handleFiles(file, holder) {
+  const reader = new FileReader();
+
+  reader.onload = function () {
+    const img = new Image();
+
+    img.src = reader.result;
+
+    holder.append(img);
+  };
+
+  reader.readAsDataURL(file.files[0]);
+}
+
+homeImgInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  handleFiles(homeImgInput, homeLogoImg);
+});
+
+opponentImgInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  handleFiles(opponentImgInput, opponentLogoImg);
+});
+
 function addMatch() {
   if (selectType.value === "match") {
+    showValid(eventNameInput);
+    showValid(opponentImgInput);
+    showValid(homeImgInput);
+    showValid(homeNameInput);
+    showValid(opponentNameInput);
+    showValid(matchDatePicker);
+    alertZero();
+
+    if (
+      eventNameInputMsg.classList.contains("bad") ||
+      opponentImgInputMsg.classList.contains("bad") ||
+      homeImgInputMsg.classList.contains("bad") ||
+      personNeededEl.classList.contains("bad") ||
+      homeNameInputMsg.classList.contains("bad") ||
+      opponentNameInput.classList.contains("bad") ||
+      matchDatePickerMsg.classList.contains("bad")
+    ) {
+      return;
+    } else {
+      const newMatch = new Match(
+        eventNameInput.value,
+        homeNameInput.value,
+        opponentNameInput.value,
+        homeLogoImg.children[0].src,
+        opponentLogoImg.children[0].src,
+        +personNeededEl.value,
+        matchDatePicker.value,
+        description.value
+      );
+
+      newMatch.addMatchToArray(newMatch);
+      newMatch.addMatchToDOM();
+
+      clearInputs(matchFormFields);
+      clearDescription();
+    }
   }
 }
 
+/*
+function moveToPopup(e) {
+  if (!map) return;
+
+  const workoutEl = e.target.closest(".workout");
+
+  if (!workoutEl) return;
+
+  const workout = this.workouts.find(
+    (work) => work.id === workoutEl.dataset.id
+  );
+
+  this.map.setView(workout.coords, this.mapZoomLevel, {
+    animate: true,
+    pan: {
+      duration: 1,
+    },
+  });
+}*/
+
 function addPlace() {
   if (selectType.value === "academy" || selectType.value === "playground") {
-    if (isNaN(+latidudeInput.value) || latidudeInput.value.trim("") === "") {
-      showEmpty(latidudeInput);
-    }
+    showValid(placeNameInput);
+    numberValidation(latidudeInput, /^-?([0-8]?[0-9]|90)(\.[0-9]{1,10})$/);
+    numberValidation(
+      longitudeInput,
+      /^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$/
+    );
 
-    if (isNaN(+longitudeInput.value) || longitudeInput.value.trim("") === "") {
-      showEmpty(longitudeInput);
+    if (
+      latidudeInputMsg.classList.contains("bad") ||
+      longitudeInputMsg.classList.contains("bad") ||
+      placeNameInputMsg.classList.contains("bad")
+    ) {
+      return;
     } else {
-      removeBadClass(latidudeInput);
-      removeBadClass(longitudeInput);
-      removeBadClass(placeNameInput);
-
       const newPlace = new Place(
         +latidudeInput.value,
         +longitudeInput.value,
         placeNameInput.value,
         description.value
       );
+
+      newPlace.addPlaceToArray(newPlace);
+      addToDOM();
 
       console.log(newPlace);
       clearInputs(placeFormFields);
@@ -400,10 +615,16 @@ function addPlace() {
   }
 }
 
+function testFunction(file) {
+  const reader = new FileReader();
+  reader.readAsDataURL(file.files[0]);
+}
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   addPlace();
   addEvent();
+  addMatch();
 });
 
 increaseBtn.addEventListener("click", increasePlayers);
